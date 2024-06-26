717058ffe41e7db00dd7210869523d298e322311
ko44d <koyoshid622@gmail.com>
2024-06-26 16:31:08 +0900

    特定の残業時間の合計して、表示するように修正

--- pkg/controller/employee_overtime_controller.go
+++ pkg/controller/employee_overtime_controller.go
@@ -1,10 +1,14 @@
 package controller
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/ko44d/hrmos-time-aggregator/pkg/dto"
 	"github.com/ko44d/hrmos-time-aggregator/pkg/usecase"
 	"net/http"
+	"regexp"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -19,6 +23,10 @@ type employeeOvertimeController struct {
 func NewEmployeeOvertimeController(womu usecase.WorkOutputsMonthlyUsecase) EmployeeOvertimeController {
 	return &employeeOvertimeController{womu: womu}
 }
+
+var timeFormat = regexp.MustCompile(`^\d{1,2}:\d{2}$`)
+
+var pattern = regexp.MustCompile(`^(直行直帰|直行|直帰)\(残業有\)$`)
 
 func (eoc *employeeOvertimeController) Aggregate(ctx *gin.Context) {
 	token, err := ctx.Cookie("token")
@@ -37,14 +45,39 @@ func (eoc *employeeOvertimeController) Aggregate(ctx *gin.Context) {
 		monthly = currentTime.Format("2006-01")
 	}
 
-	query := dto.NewWorkOutputsMonthlyQuery(token, companyURL, monthly, 0, 0, 0, "", "")
+	query := dto.NewWorkOutputsMonthlyQuery(token, companyURL, monthly, 7, 31, 1, "", "")
 	data, err := eoc.womu.Get(query)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
+
+	var totalMinutes int
+	for _, d := range data {
+		if pattern.MatchString(d.SegmentTitle) {
+			if !timeFormat.MatchString(d.TotalOverWorkTime) {
+				ctx.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Errorf("invalid time format")})
+			}
+			parts := strings.Split(d.TotalOverWorkTime, ":")
+			hours, err := strconv.Atoi(parts[0])
+			if err != nil {
+				ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			}
+			minutes, err := strconv.Atoi(parts[1])
+			if err != nil {
+				ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			}
+			totalMinutes += hours*60 + minutes
+		}
+	}
+
+	hours := totalMinutes / 60
+	minutes := totalMinutes % 60
+
 	ctx.HTML(http.StatusOK, "work_outputs.html", gin.H{
 		"data":    data,
 		"monthly": monthly,
+		"hours":   hours,
+		"minutes": minutes,
 	})
 }
