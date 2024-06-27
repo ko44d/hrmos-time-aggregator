5dde0222df9ed970f0971d0d24babc4ce7524996
ko44d <koyoshid622@gmail.com>
2024-06-27 10:36:04 +0900

    特定の人の合計残業時間を計算する仕組みを作成

--- pkg/controller/employee_overtime_controller.go
+++ pkg/controller/employee_overtime_controller.go
@@ -1,8 +1,10 @@
 package controller
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/ko44d/hrmos-time-aggregator/pkg/dto"
+	"github.com/ko44d/hrmos-time-aggregator/pkg/repository"
 	"github.com/ko44d/hrmos-time-aggregator/pkg/usecase"
 	"net/http"
 	"strconv"
@@ -22,6 +24,10 @@ func NewEmployeeOvertimeController(womu usecase.WorkOutputsMonthlyUsecase) Emplo
 	return &employeeOvertimeController{womu: womu}
 }
 
+var userid_list = []int{
+	7, 12, 14, 21, 24,
+}
+
 func (eoc *employeeOvertimeController) Aggregate(ctx *gin.Context) {
 	token, err := ctx.Cookie("token")
 	if err != nil {
@@ -39,41 +45,53 @@ func (eoc *employeeOvertimeController) Aggregate(ctx *gin.Context) {
 		monthly = currentTime.Format("2006-01")
 	}
 
-	query := dto.NewWorkOutputsMonthlyQuery(token, companyURL, monthly, 7, 31, 1, "", "")
-	data, err := eoc.womu.Get(query)
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
+	dailyWorkDatas := []struct {
+		DailyWorkData []repository.DailyWorkData
+		TotalOvertime string
+	}{}
 
-	totalOvertimeMinutes := 0
-	for _, record := range data {
-		if record.SegmentTitle == "直行直帰(残業有)" || record.SegmentTitle == "直行(残業有)" || record.SegmentTitle == "直帰(残業有)" {
-			parts := strings.Split(record.TotalOverWorkTime, ":")
-			if len(parts) == 2 {
-				hours, err := strconv.Atoi(parts[0])
-				if err != nil {
-					ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	var totalOvertime string
+	for _, ui := range userid_list {
+		query := dto.NewWorkOutputsMonthlyQuery(token, companyURL, monthly, ui, 31, 1, "", "")
+		data, err := eoc.womu.Get(query)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		totalOvertimeMinutes := 0
+		for _, record := range data {
+			if record.SegmentTitle == "直行直帰(残業有)" || record.SegmentTitle == "直行(残業有)" || record.SegmentTitle == "直帰(残業有)" {
+				parts := strings.Split(record.TotalOverWorkTime, ":")
+				if len(parts) == 2 {
+					hours, err := strconv.Atoi(parts[0])
+					if err != nil {
+						ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+					}
+					minutes, err := strconv.Atoi(parts[1])
+					if err != nil {
+						ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+					}
+					totalOvertimeMinutes += hours*60 + minutes
 				}
-				minutes, err := strconv.Atoi(parts[1])
-				if err != nil {
-					ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-				}
-				totalOvertimeMinutes += hours*60 + minutes
 			}
 		}
-	}
 
-	totalHours := totalOvertimeMinutes / 60
-	totalMinutes := totalOvertimeMinutes % 60
-	totalOvertime := "0:00"
-	if totalOvertimeMinutes > 0 {
-		totalOvertime = strconv.Itoa(totalHours) + ":" + strconv.Itoa(totalMinutes)
-	}
+		totalHours := totalOvertimeMinutes / 60
+		totalMinutes := totalOvertimeMinutes % 60
+		totalOvertime = strconv.Itoa(totalHours) + ":" + fmt.Sprintf("%02d", totalMinutes)
 
+		dailyWorkData := struct {
+			DailyWorkData []repository.DailyWorkData
+			TotalOvertime string
+		}{
+			DailyWorkData: data,
+			TotalOvertime: totalOvertime,
+		}
+
+		dailyWorkDatas = append(dailyWorkDatas, dailyWorkData)
+	}
 	ctx.HTML(http.StatusOK, "work_outputs.html", gin.H{
-		"data":          data,
-		"monthly":       monthly,
-		"totalOvertime": totalOvertime,
+		"dailyWorkDatas": dailyWorkDatas,
+		"monthly":        monthly,
 	})
 }
